73a1d840a6fb8fd973dda7135466cab5e8fc3bd2
leopoldkwok <leopoldkwok@gmail.com>
2014-11-19 16:22:01 +0000

    added columns to likes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119160548) do
+ActiveRecord::Schema.define(version: 20141119162125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20141119160548) do
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "user_id"
   end
 
   add_index "likes", ["post_id"], name: "index_likes_on_post_id", using: :btree
@@ -61,6 +62,8 @@ ActiveRecord::Schema.define(version: 20141119160548) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
