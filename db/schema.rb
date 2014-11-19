f6a8683459f2ff46c6a80bd40276e4e8dec37abc
leopoldkwok <leopoldkwok@gmail.com>
2014-11-19 16:51:38 +0000

    add post id to comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119162952) do
+ActiveRecord::Schema.define(version: 20141119165056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20141119162952) do
     t.text     "thoughts"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "post_id"
   end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "likes", force: true do |t|
     t.integer  "post_id"
